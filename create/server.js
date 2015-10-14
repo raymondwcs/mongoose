ab3ec9244b21233a7668a8e72d04bab672e76b19
Raymond SO <rso@ouhk.edu.hk>
2015-10-14 10:16:38 +0800

    create sample

---
+++ create/server.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var kittySchema = mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+	var Kitten = mongoose.model('Kitten', kittySchema);
+	var fluffy = new Kitten({name: 'fluffy', age: 0});
+
+	fluffy.save(function(err) {
+		if (err) throw err
+		console.log('Kitten created!')
+		db.close();
+	});
+});
