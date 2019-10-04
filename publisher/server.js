dd3d883d2ac28d993797e803f675a1bd0aa29d88
Raymond So <rso@ouhk.edu.hk>
2019-10-04 09:29:46 +0800

    updated to support mongoose 4.13.19

--- publisher/server.js
+++ publisher/server.js
@@ -1,22 +1,25 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://',
+				 {useMongoClient: true,}
+);
 
-var publisherSchema = require('./models/publisher');
-var db = mongoose.connection;
+const publisherSchema = require('./models/publisher');
+const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-	var Publisher = mongoose.model('Publisher', publisherSchema);
+db.once('open', (callback) => {
+	let Publisher = mongoose.model('Publisher', publisherSchema);
 
-	var p = new Publisher({name: 'ABC', address: '30 Good Shepherd Street'});
+	let p = new Publisher({name: 'ABC', address: '30 Good Shepherd Street'});
 	p.books.push({isbn: '01234567890ABC',
 	              title: 'Introduction to Node.JS',
 	              author: 'John Smith',
 	              price: 70.00,
 	              stock: 0,
-	              available: false});
+				  available: false
+	});
 
-	p.save(function(err) {
+	p.save((err) => {
 		if (err) throw err
 		console.log('Publisher created!')
 		db.close();
