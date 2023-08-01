6266dae3ce4aceb039f0f36236410fcf07704abe
Raymond SO <raymond.wc.so@gmail.com>
2023-08-01 09:09:28 +0800

    updated

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const mongoose = require('mongoose');
 const dbName = 'test'
 const mongouri = `mongodb://172.17.0.3/${dbName}`;
 
+const maxNumberOfPhone = 10;
+const phoneLimit = (val) => {
+    return val.length <= maxNumberOfPhone && val.length > 0;
+}
 
 const contactSchema = mongoose.Schema({
     name: { type: String, required: true, minlength: 1, maxlength: 20 },
     birthyear: { type: Number, min: 1900, max: 2100 },
-    phone: [{
-        type: { type: String, enum: ['office', 'home', 'mobile'], default: 'mobile', required: true },
-        number: { type: String, required: true }
-    }],
-    email: String
+    phone: {
+        type: [{
+            phoneType: { type: String, enum: ['office', 'home', 'mobile'], default: 'mobile', required: true },
+            number: { type: String, required: true }
+        }],
+        validate: [phoneLimit, `Number of {PATH} exceeds the limit of ${maxNumberOfPhone}} or missing`]
+    },
+    email: String,
 });
 
-const create = async () => {
+const create = async (contact) => {
     await mongoose.connect(mongouri);
 
     // create a contact
     const Contact = mongoose.model('contact', contactSchema);
-    const raymond = new Contact({ name: 'Raymond', phone: [{ type: 'mobile', number: '12345678' }] });
+    const raymond = new Contact(contact);
     const results = await raymond.save();
 
     return results;
@@ -47,7 +54,9 @@ const del = async () => {
     return results;
 }
 
-create().then((results) => {
+let contact = { name: 'Raymond', phone: [{ phoneType: 'mobile', number: '12345678' }] };
+
+create(contact).then((results) => {
     console.log('created 1 document:')
     console.log(util.inspect(results, { showHidden: true, depth: null, colors: true }));
     read().then((results) => {
@@ -58,8 +67,18 @@ create().then((results) => {
             console.log(util.inspect(results, { showHidden: true, depth: null, colors: true }));
             del().then((results) => {
                 console.log(`deleted ${results['deletedCount']} document(s)`);
-                mongoose.disconnect();
+                mongoose.disconnect().then(() => {
+                    console.log('closed mongodb connection');
+                    contact = { name: 'Mary' }
+                    console.log('create 1 document without a phone!!!')
+                    create(contact).then(results => {
+                        console.log(util.inspect(results, { showHidden: true, depth: null, colors: true }));
+                        mongoose.disconnect().then(() => console.log('closed mongodb connection')
+                        );
+                    });
+                });
             });
         })
     })
 }).catch(err => console.log(err));
+
